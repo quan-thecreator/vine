0d17804bdc64e3a325a56607207f351350e04fd8
quan-thecreator <65472328+quan-thecreator@users.noreply.github.com>
2022-03-08 22:13:36 -0600

    Create vine.sh

---
+++ vine.sh
@@ -0,0 +1,7 @@
+echo Hello, child.
+sudo apt-get install sox libsox-fmt-all
+wget -O vine-boom.mp3 https://www.myinstants.com/media/sounds/vine-boom.mp3
+for VARIABLE in 1 2 3 4 5 6 7 8 9 10
+do 
+play vineboom.mp3
+done
